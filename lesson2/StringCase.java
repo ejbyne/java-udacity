ce91e7f2995c2537db663cabb65613e1f1e3b34f
Edward Byne <ejbyne@gmail.com>
2015-04-07 09:27:32 +0200

    Halfway through lesson 2; playing with String methods

---
+++ lesson2/StringCase.java
@@ -0,0 +1,11 @@
+public class StringCase
+{
+  public static void main(String[] args)
+  {
+    String river = "Mississippi";
+    System.out.println(river.toUpperCase());
+    System.out.println(river.toLowerCase());
+    System.out.println(river.toUpperCase().toLowerCase());
+    System.out.println(river);
+  }
+}
---
+++ lesson2/StringReplace.java
@@ -0,0 +1,9 @@
+public class StringReplace
+{
+  public static void main(String[] args)
+  {
+    String river = "Mississippi";
+    String newRiver = river.replace("i", "x");
+    System.out.println(newRiver);
+  }
+}
