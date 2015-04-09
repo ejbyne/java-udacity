a7423634c227be09d1275248c2e9ae578681d720
Edward Byne <ejbyne@gmail.com>
2015-04-09 16:01:00 +0200

    Adds CarDemo class and Person class

---
+++ lesson3/cars5/CarDemo.java
@@ -0,0 +1,7 @@
+public class CarDemo
+{
+  public static void main(String[] args)
+  {
+    Car hummer = new Car(5, "hummer.jpg");
+  }
+}
---
+++ lesson3/friends/Person.java
@@ -0,0 +1,26 @@
+public class Person
+{
+  private String name;
+  private String friends;
+
+  public Person(String aName)
+  {
+    name = aName;
+    friends = "";
+  }
+
+  public String getFriends()
+  {
+    return friends;
+  }
+
+  public void addFriend(Person friend)
+  {
+    friends = friends + " " + friend.name;
+  }
+
+  public void unfriend(Person nonFriend)
+  {
+    friends = friends.replace(" " + nonFriend.name, "");
+  }
+}
