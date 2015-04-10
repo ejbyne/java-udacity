d958823c25b91d23d0228b41f93ec559c75ada1b
Edward Byne <ejbyne@gmail.com>
2015-04-10 22:34:05 +0200

    Adds InchWorm class and tester class

---
+++ lesson3/inchWorm/InchWorm.java
@@ -0,0 +1,37 @@
+public class InchWorm
+{
+  private int position;
+  private int direction;
+
+  public InchWorm()
+  {
+    position = 0;
+    direction = 1;
+  }
+
+  public InchWorm(int initialPosition)
+  {
+    position = initialPosition;
+    direction = 1;
+  }
+
+  public void move()
+  {
+    position = position + direction;
+  }
+
+  public void turn()
+  {
+    direction = -direction;
+  }
+
+  public int getPosition()
+  {
+    return position;
+  }
+
+  public int getDirection()
+  {
+    return direction;
+  }
+}
