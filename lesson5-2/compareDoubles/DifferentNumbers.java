f16e87ce23abde2cb1198da06290c7bfd7f176eb
Edward Byne <ejbyne@gmail.com>
2015-04-13 09:13:21 +0200

    Adds EqualNumbers and EqualStrings classes

---
+++ lesson5-2/compareDoubles/DifferentNumbers.java
@@ -0,0 +1,18 @@
+public class DifferentNumbers
+{
+  public static void main(String[] args)
+  {
+    double original = 2;
+    double root = Math.sqrt(original);
+    double rootSquared = root * root;
+    if (rootSquared == original)
+    {
+      System.out.println("They are the same");
+    }
+    else
+    {
+      System.out.println("rootSquared is "
+        + rootSquared);
+    }
+  }
+}
