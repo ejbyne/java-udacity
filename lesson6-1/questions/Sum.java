0d0350b7ea29c44cc3a590b9b6a1a2588fbc5b04
Edward Byne <ejbyne@gmail.com>
2015-04-14 17:37:31 +0200

    Adds Sum class

---
+++ lesson6-1/questions/Sum.java
@@ -0,0 +1,24 @@
+
+/**
+ * Complete the method to find sum of all the positive even numbers less than or equal to the given number
+ * use a loop
+ */
+public class Sum
+{
+   /**
+    * Gets the sum of all the even numbers less than or equal to the given number
+    * @param n the given number, the ceiling
+    * @return the sum of all the even numbers less that the ceiling
+    */
+   public int sum(int n)
+   {
+      int sum = 0;
+      int evenNumber = 2;
+      while (evenNumber <= n)
+      {
+          sum += evenNumber;
+          evenNumber += 2;
+      }
+      return sum;
+   }
+}
