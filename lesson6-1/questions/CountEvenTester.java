41dff552bd9c94d2de6d052c8ad97f78da0e3325
Edward Byne <ejbyne@gmail.com>
2015-04-14 17:48:03 +0200

    Adds CountEvenTester class

---
+++ lesson6-1/questions/CountEvenTester.java
@@ -0,0 +1,28 @@
+//hw04_20
+/**
+ * Complete the class. Using a Scanner, ask the user to enter a series of integers
+ * and count the number of even numbers
+ * Use a loop. Do not let bad input (a non-integer) terminate your program with an error.
+ * When the user enters any non-integer, print the number of even integers entered and quit.
+ * Hint: remember hasNextInt()
+ * Hint: use the % operator to determine if a number is even
+ */
+import java.util.Scanner;
+public class CountEvenTester
+{
+   public static void main(String[] args)
+   {
+       Scanner in = new Scanner(System.in);
+       System.out.print("Enter an integer: ");
+       int count = 0;
+       while (in.hasNextInt())
+       {
+          if (in.nextInt() % 2 == 0)
+          {
+              count ++;
+          }
+          System.out.print("Enter an integer: ");
+       }
+       System.out.println(count);
+   }
+}
