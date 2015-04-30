98a17be46449887b8dd4c0d4b228ef30bf3d7576
Edward Byne <ejbyne@gmail.com>
2015-04-30 16:15:15 +0100

    Adds gallery 5 and gallery 6 ListOfPictures files

---
+++ lesson7-1/gallery6/ListOfPictures.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class ListOfPictures
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Picture> gallery = new ArrayList<Picture>();
+        gallery.add(new Picture("degas1.jpg"));
+        gallery.add(new Picture("guigou1.jpg"));
+        gallery.add(new Picture("gaugin1.jpg"));
+        gallery.add(new Picture("monet1.jpg"));
+        gallery.add(new Picture("seurat1.jpg"));
+        
+        Picture tallest = gallery.get(0);
+        for (Picture pic : gallery)
+        {
+            if (pic.getHeight() > tallest.getHeight())
+            {
+                tallest = pic;
+            }
+        }
+
+        System.out.println("Tallest height: " 
+           + tallest.getHeight());
+        tallest.draw();
+    }
+}
